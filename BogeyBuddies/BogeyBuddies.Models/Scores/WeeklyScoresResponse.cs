95fc4407d92a48e1b67a55daa18503a9c9075e04
colby <colby.ron.carlson@gmail.com>
2023-11-12 14:55:26 -0600

    Commit front-end working.

---
+++ BogeyBuddies/BogeyBuddies.Models/Scores/WeeklyScoresResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace BogeyBuddies.Models.Scores
+{
+    public class WeeklyScoreResponse
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string Type { get; set; }
+        public string WeekIdentifier { get; set; }
+        public DateTime Date { get; set; }
+        public int GrossScore { get; set; }
+        public int Handicap { get; set; }
+        public int NetScore { get; set; }
+        public decimal WeeklyWinnings { get; set; }
+    }
+}
